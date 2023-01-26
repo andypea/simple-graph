3c99032a1b7c759ffa4c888746ed84536cc9fbbc
Andrew Punnett <andrew.punnett@gmail.com>
2023-01-27 11:55:29 +1300

    Make vertices move and freeze when clicked.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
@@ -13,12 +14,58 @@ function App() {
 }
 
 function SimpleGraph(props) {
+    // TODO: Update verticesPositions when props.vertices changes.
+    // TODO: Copy all properties across when copying objects.
+
+    const [verticesPositions, setVerticesPositions] = useState(props.vertices.map((v) => ({
+        key: v.key, 
+        cx: Math.random() * props.width, 
+        cy: Math.random() * props.height,
+        frozen: false 
+    })));
+
+    useEffect(() => {
+        let frameId = null;
+
+        function onFrame() {
+            setVerticesPositions((oldVerticesPositions) => oldVerticesPositions.map((v) => ({
+                key: v.key,
+                cx: (v.cx + (v.frozen ? 0 : 1) * 0.5) % props.width,
+                cy: (v.cy + (v.frozen ? 0 : 1) * 0.5) % props.height,
+                frozen: v.frozen
+            })));
+
+            frameId = requestAnimationFrame(onFrame);
+        }
+
+        function start() {
+            frameId = requestAnimationFrame(onFrame);
+        }
+
+        function stop() {
+            cancelAnimationFrame(frameId);
+            frameId = null;
+        }
+
+        start();
+        return () => stop();
+    }, [props.width, props.height]);
+
+    const toggleFreeze = (key) => {
+        setVerticesPositions(oldVerticesPositions => oldVerticesPositions.map(v => ({
+            key: v.key,
+            cx: v.cx,
+            cy: v.cy,
+            frozen: v.key === key ? (! v.frozen) : v.frozen
+        })));
+    }
+    
     return (
         <svg width={props.width} height={props.height}>
             <rect x="0" y="0" width={props.width} height={props.height} fill="lightgrey" />
             <g>
                 {
-                    props.vertices.map((v) => <circle key={v.key} cx={Math.random() * props.width} cy={Math.random() * props.height} r="5" />)
+                    verticesPositions.map((v) => <circle key={v.key} cx={v.cx} cy={v.cy} r="10" onPointerDown={() => toggleFreeze(v.key)} />)
                 }
             </g>
         </svg>
@@ -26,3 +73,4 @@ function SimpleGraph(props) {
 }
 
 export default App;
+
