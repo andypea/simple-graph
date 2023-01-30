eaf34baad030b85234d65801c8f9ff77c83da4ef
Andrew Punnett <andrew.punnett@gmail.com>
2023-01-30 14:49:24 +1300

    Remove prototype draggable element.

--- src/App.js
+++ src/App.js
@@ -67,7 +67,6 @@ function SimpleGraph(props) {
                     verticesPositions.map((v) => <Vertex key={v.id} id={v.id} cx={v.cx} cy={v.cy} moveVertex={moveVertex} />)
                 }
             </g>
-            <Draggable />
         </svg>
     );
 }
@@ -114,68 +113,6 @@ const Vertex = (props) => {
                    ref={thisVertex}
                    />
 }
-
-const Draggable = (props) => {
-    // TODO: Does this work on Safari (I believe it has a bug in `getScreenCTM`.
-
-    const thisVertex = useRef(null);
-
-    const [position, setPosition] = useState({x:0, y:0});
-    const [colour, setColour] = useState("red");
-    const [dragging, setDragging] = useState(false);
-    const [offset, setOffset] = useState({x: 0, y: 0});
-
-    const handleOnPointerDown = (event) => {
-        thisVertex.current.setPointerCapture(event.pointerId);
-        
-        setDragging(true);
-            
-        const screenToLocalTransformationMatrix = thisVertex.current.getScreenCTM();
-        const pointerScreenPosition = new DOMPointReadOnly(event.pageX, event.pageY);
-        const pointerLocalPosition = pointerScreenPosition.matrixTransform(screenToLocalTransformationMatrix.inverse());
-        setOffset({x: pointerLocalPosition.x - position.x, y: pointerLocalPosition.y - position.y});
-    };
-    
-    const handleOnPointerMove = (event) => {
-        if (dragging) {
-            setColour(getRandomColour());
-
-            const screenToLocalTransformationMatrix = thisVertex.current.getScreenCTM();
-            const pointerScreenPosition = new DOMPointReadOnly(event.pageX, event.pageY);
-            const pointerLocalPosition = pointerScreenPosition.matrixTransform(screenToLocalTransformationMatrix.inverse());
-            setPosition({x: pointerLocalPosition.x - offset.x, y: pointerLocalPosition.y - offset.y});
-        }
-    };
-    
-    const handleOnPointerUp = (event) => {
-        setDragging(false);
-        thisVertex.current.releasePointerCapture(event.pointerId);
-    };
-
-    return <rect x={position.x} 
-                 y={position.y} 
-                 width="40" 
-                 height="40" 
-                 fill={colour} 
-                 onPointerDown={handleOnPointerDown} 
-                 onPointerMove={handleOnPointerMove} 
-                 onPointerUp={handleOnPointerUp} 
-                 ref={thisVertex}
-    />
-}
-
-const getRandomColour = () => {
-    const colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-    return colours[getRandomInt(0, colours.length - 1)];
-};
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
-
 
 export default App;
 
