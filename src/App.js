7945d9f2e6e136401667c40ad2f5b7d938658e05
Andrew Punnett <andrew.punnett@gmail.com>
2023-01-31 18:10:49 +1300

    Store the state in a Map, not an Array.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { useState, useEffect, useRef } from 'react';
 function App() {
   return (
     <div className="App">
-        <SimpleGraph width="500" height="500" vertices={[
-            {id: "One"},
-            {id: "Two"},
-            {id: "Three"},
-        ]} />
+        <SimpleGraph width="500" 
+                     height="500" 
+                     vertices={[
+                         {id: "One"},
+                         {id: "Two"},
+                         {id: "Three"}]}
+                     edges={[
+                         {id: "OneTwo", vertexA: "One", vertexB: "Two", length: 1},
+                         {id: "TwoThree", vertexA: "Two", vertexB: "Three", length: 2},
+        ]}/>
     </div>
   );
 }
@@ -19,23 +24,34 @@ function SimpleGraph(props) {
     // TODO: Bug: if SimpleGraph width or height is specified as a percentage.
     // TODO: Check is the dragging works correctly in Safari.
     // TODO: Vertices shouldn't need to pass their own ID to the move, freeze, unfreeze, etc. functions.
+    // TODO: Extract the simulation data and code from the React component and make the functions more clearly pure.
 
-    const [verticesPositions, setVerticesPositions] = useState(props.vertices.map((v) => ({
-        id: v.id, 
+    const [verticesPositions, setVerticesPositions] = useState(new Map(props.vertices.map((v) => ([
+        v.id, 
+        {
         cx: Math.random() * props.width, 
         cy: Math.random() * props.height,
+        vx: 0,
+        vy: 0,
         frozen: false 
-    })));
+    }]))));
 
+    const timeStep = 0.005;
+    const friction = 0.01;
+    
     useEffect(() => {
         let frameId = null;
 
         function onFrame() {
-            setVerticesPositions((oldVerticesPositions) => oldVerticesPositions.map((v) => ({
-                ...v,
-                cx: (v.cx + (v.frozen ? 0 : 1) * 0.5) % props.width,
-                cy: (v.cy + (v.frozen ? 0 : 1) * 0.5) % props.height,
-            })));
+            setVerticesPositions(
+                (oldVerticesPositions) => new Map([...oldVerticesPositions.entries()].map(entry => [entry[0], {
+                    ...entry[1],
+                    cx: (entry[1].cx + (entry[1].frozen ? 0 : 1) * timeStep * entry[1].vx) % props.width,
+                    cy: (entry[1].cy + (entry[1].frozen ? 0 : 1) * timeStep * entry[1].vy) % props.height,
+                    vx: entry[1].frozen ? 0 : (entry[1].vx - timeStep * Math.sign(entry[1].cx - props.width / 2) * Math.pow(entry[1].cx - props.width / 2, 2) - friction * entry[1].vx),
+                    vy: entry[1].frozen ? 0 : (entry[1].vy - timeStep * Math.sign(entry[1].cy - props.width / 2) * Math.pow(entry[1].cy - props.width / 2, 2) - friction * entry[1].vy)
+                }]))
+            );
 
             frameId = requestAnimationFrame(onFrame);
         }
@@ -54,40 +70,48 @@ function SimpleGraph(props) {
     }, [props.width, props.height]);
 
     const moveVertex = (id, position) => {
-            setVerticesPositions((oldVerticesPositions) => oldVerticesPositions.map((v) => ({
-                ...v,
-                cx: (v.id === id) ? position.x : v.cx, 
-                cy: (v.id === id) ? position.y : v.cy, 
-            })));
-    }
+            setVerticesPositions((oldVerticesPositions) => new Map(oldVerticesPositions.entries()).set(id, {...oldVerticesPositions.get(id), cx: position.x, cy: position.y}))
+    };
 
     const freezeVertex = (id) => {
-        setVerticesPositions((oldVerticesPositions) => oldVerticesPositions.map((v) => ({
-            ...v,
-            frozen: (v.id === id) ? true : v.frozen
-        })));
+            setVerticesPositions((oldVerticesPositions) => new Map(oldVerticesPositions.entries()).set(id, {...oldVerticesPositions.get(id), frozen: true}))
     };
     
     const unfreezeVertex = (id) => {
-        setVerticesPositions((oldVerticesPositions) => oldVerticesPositions.map((v) => ({
-            ...v,
-            frozen: (v.id === id) ? false : v.frozen
-        })));
+            setVerticesPositions((oldVerticesPositions) =>  new Map(oldVerticesPositions.entries()).set(id, {...oldVerticesPositions.get(id), frozen: false}))
     };
 
     return (
         <svg width={props.width} height={props.height}>
             <rect x="0" y="0" width={props.width} height={props.height} fill="lightgrey" />
             <g>
-                {
-                    verticesPositions.map((v) => <Vertex key={v.id} 
-                                                         id={v.id} 
-                                                         cx={v.cx} 
-                                                         cy={v.cy} 
-                                                         moveVertex={moveVertex} 
-                                                         freezeVertex={freezeVertex}
-                                                         unfreezeVertex={unfreezeVertex}/>)
-                }
+                <g>
+                    {
+                        props.edges.map(e => {
+                            return (
+                                <Edge 
+                                    key={e.id} 
+                                    // TODO: Don't pass the whole object here.
+                                    positionA={verticesPositions.get(e.vertexA)} 
+                                    positionB={verticesPositions.get(e.vertexB)}
+                                />
+                            )
+                    }) 
+                    }
+                </g>
+                <g>
+                    {
+                        [...verticesPositions.entries()].map(([id, v]) => <Vertex 
+                            key={id} 
+                            id={id} 
+                            cx={v.cx} 
+                            cy={v.cy} 
+                            moveVertex={moveVertex} 
+                            freezeVertex={freezeVertex}
+                            unfreezeVertex={unfreezeVertex}
+                            />)
+                    }
+                </g>
             </g>
         </svg>
     );
@@ -136,6 +160,19 @@ const Vertex = (props) => {
                    onPointerUp={handleOnPointerUp} 
                    ref={thisVertex}
                    />
+}
+
+const Edge = (props) => {
+    // TODO: Refactor so that these positions are just `x` and `y`, not `cx` and `cy`.
+    return (
+        <line 
+            x1={props.positionA.cx} 
+            y1={props.positionA.cy} 
+            x2={props.positionB.cx} 
+            y2={props.positionB.cy} 
+            stroke="black"
+        />
+    );
 }
 
 export default App;
