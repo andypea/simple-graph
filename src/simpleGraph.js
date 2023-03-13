93d91de864c1055f2b7eca114047aafad3db0281
Andrew Punnett <andrew.punnett@gmail.com>
2023-03-13 19:00:33 +1300

    Remove redundant code.

--- src/simpleGraph.js
+++ src/simpleGraph.js
@@ -1,124 +1,7 @@
 import { useState, useEffect, useRef } from "react";
+import { updateVerticesPositions } from "./numericalSimulation.js";
 
-const reconcileVertexPositions = (
-  vertices,
-  oldVerticesPositions,
-  width,
-  height
-) => {
-  const newVerticesPositions = new Map();
-
-  for (const vertex of vertices) {
-    if (oldVerticesPositions.has(vertex.id)) {
-      newVerticesPositions.set(vertex.id, oldVerticesPositions.get(vertex.id));
-    } else {
-      newVerticesPositions.set(vertex.id, {
-        cx: Math.random() * width,
-        cy: Math.random() * height,
-        vx: 0,
-        vy: 0,
-        frozen: false,
-      });
-    }
-  }
-
-  return newVerticesPositions;
-};
-
-const updateVerticesPositions = (
-  oldVerticesPositions,
-  width,
-  height,
-  friction,
-  timeStep,
-  edges,
-  springConstant,
-  vertices
-) => {
-  // First create a new Map containing all the vertices listed.
-  const newVerticesPositions = reconcileVertexPositions(
-    vertices,
-    oldVerticesPositions,
-    width,
-    height
-  );
-
-  const forces = new Map(vertices.map((vertex) => [vertex.id, { x: 0, y: 0 }]));
-
-  for (const vertex of vertices) {
-    const vertexPosition = newVerticesPositions.get(vertex.id);
-    const force = forces.get(vertex.id);
-
-    forces.set(vertex.id, {
-      x: force.x - friction * vertexPosition.vx,
-      y: force.y - friction * vertexPosition.vy,
-    });
-  }
-
-  for (const e of edges) {
-    if (
-      !newVerticesPositions.has(e.source) ||
-      !newVerticesPositions.has(e.target)
-    ) {
-      continue;
-    }
-
-    // TODO: Check both vertices exist!
-    const source = newVerticesPositions.get(e.source);
-    const target = newVerticesPositions.get(e.target);
-
-    const distance = Math.sqrt(
-      Math.pow(target.cx - source.cx, 2) + Math.pow(target.cy - source.cy, 2)
-    );
-    const forceScalar = springConstant * (distance - e.length);
-
-    const forceSourceToTarget = {
-      x: (forceScalar * (target.cx - source.cx)) / distance,
-      y: (forceScalar * (target.cy - source.cy)) / distance,
-    };
-
-    const forceSource = forces.get(e.source);
-    forces.set(e.source, {
-      x: forceSource.x + forceSourceToTarget.x,
-      y: forceSource.y + forceSourceToTarget.y,
-    });
-
-    const forceTarget = forces.get(e.target);
-    forces.set(e.target, {
-      x: forceTarget.x - forceSourceToTarget.x,
-      y: forceTarget.y - forceSourceToTarget.y,
-    });
-  }
-
-  for (const vertex of vertices) {
-    const oldPosition = newVerticesPositions.get(vertex.id);
-    newVerticesPositions.set(vertex.id, {
-      cx: clamp(
-        oldPosition.cx +
-          (oldPosition.frozen ? 0 : 1) * timeStep * oldPosition.vx,
-        0,
-        width
-      ),
-      cy: clamp(
-        oldPosition.cy +
-          (oldPosition.frozen ? 0 : 1) * timeStep * oldPosition.vy,
-        0,
-        height
-      ),
-      vx: oldPosition.frozen
-        ? 0
-        : oldPosition.vx + timeStep * forces.get(vertex.id).x,
-      vy: oldPosition.frozen
-        ? 0
-        : oldPosition.vy + timeStep * forces.get(vertex.id).y,
-      frozen: oldPosition.frozen,
-    });
-  }
-
-  return newVerticesPositions;
-};
-
-export function SimpleGraph({render = (fill, label) => <VertexDisplay fill={fill} label={label} />, width = 100, height = 100, ...props} = {}) {
+export function SimpleGraph({render = (fill, label) => <DefaultVertexElement fill={fill} label={label} />, width = 100, height = 100, ...props} = {}) {
   const [verticesPositions, setVerticesPositions] = useState(new Map());
 
   const timeStep = 0.005;
@@ -297,10 +180,10 @@ const Vertex = (props) => {
   );
 };
 
-const VertexDisplay = (props) => {
+const DefaultVertexElement = (props) => {
   return (
     <g>
-      <Star outerRadius="10" fill={props.fill} />
+      <circle r="5" fill={props.fill} stroke="black" />
       <text
         dominantBaseline="middle"
         textAnchor="middle"
@@ -334,45 +217,4 @@ const Edge = (props) => {
     />
   );
 };
-
-const Star = ({
-  outerRadius = 10,
-  innerRadiusRatio = 0.5,
-  numPoints = 5,
-  fill = "yellow",
-}) => {
-  const theta = Math.PI / numPoints;
-  const angularPoints = Array.from(Array(2 * numPoints), (_, i) => ({
-    angle: i * theta,
-    r: i % 2 === 0 ? outerRadius : innerRadiusRatio * outerRadius,
-  }));
-
-  const cartesianPoints = angularPoints.map(({ angle, r }) => ({
-    x: r * Math.cos(angle),
-    y: r * Math.sin(angle),
-  }));
-
-  const pathHeadString = `M ${cartesianPoints[0].x}, ${cartesianPoints[0].y}`;
-  const pathMiddleString = cartesianPoints
-    .slice(1, cartesianPoints.length)
-    .map(({ x, y }) => `L ${x}, ${y}`)
-    .join(" ");
-  const pathTailString = "Z";
-
-  const pathString = "".concat(
-    pathHeadString,
-    " ",
-    pathMiddleString,
-    " ",
-    pathTailString
-  );
-
-  return (
-    <g>
-      <path d={pathString} fill={fill} stroke="black" />
-    </g>
-  );
-};
-
-const clamp = (x, min, max) => Math.min(Math.max(x, min), max);
 
